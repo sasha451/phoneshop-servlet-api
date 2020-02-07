31ddc115d276d4187240d4cc3fda90e9a56d4102
sasha451 <shulga.sasha.new@gmail.com>
2020-02-07 20:13:22 +0300

    Task 1: Implemented ArrayListProductDao, added productList to PLP

--- src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
@@ -1,25 +1,65 @@
 package com.es.phoneshop.model.product;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class ArrayListProductDao implements ProductDao {
+
+    private List<Product> productList = new ArrayList<>();
+
     @Override
-    public Product getProduct(Long id) {
-        throw new RuntimeException("Not implemented");
+    public synchronized Product getProduct(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("Id should not be null");
+        }
+        return findProductById(id)
+                .orElseThrow(() -> new NoSuchElementException("Product with id " + id + " was not found"));
     }
 
     @Override
-    public List<Product> findProducts() {
-        throw new RuntimeException("Not implemented");
+    public synchronized List<Product> findProducts() {
+        return productList.stream()
+                .filter(product -> product.getPrice() != null && product.getStock() > 0)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public void save(Product product) {
-        throw new RuntimeException("Not implemented");
+    public synchronized void save(Product product) {
+        if (product == null) {
+            throw new IllegalArgumentException("Product should not be null");
+        }
+        if (!productAlreadyExistenceCheck(product)) {
+            productList.add(product);
+        } else {
+            throw new IllegalArgumentException("Product with id " + product.getId() + " is already exists");
+        }
+    }
+
+    private boolean productAlreadyExistenceCheck(Product product) {
+        return productList.stream()
+                .anyMatch(productFromList -> productFromList.getId().equals(product.getId()));
+    }
+
+    List<Product> getProductList() {
+        return productList;
     }
 
     @Override
-    public void delete(Long id) {
-        throw new RuntimeException("Not implemented");
+    public synchronized void delete(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("Id should not be null");
+        }
+        findProductById(id)
+                .map(product -> productList.remove(product))
+                .orElseThrow(() -> new NoSuchElementException("Product with id " + id + " was not found"));
+    }
+
+    private Optional<Product> findProductById(Long id) {
+        return productList.stream().
+                filter(product -> product.getId().equals(id))
+                .findAny();
     }
 }
--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -2,20 +2,96 @@ package com.es.phoneshop.model.product;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import static org.junit.Assert.assertTrue;
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.NoSuchElementException;
 
-public class ArrayListProductDaoTest
-{
-    private ProductDao productDao;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class ArrayListProductDaoTest {
+    private ArrayListProductDao productDao;
+
+    @Mock
+    private Product product1;
+    @Mock
+    private Product product2;
+    @Mock
+    private Product product3;
 
     @Before
     public void setup() {
+        MockitoAnnotations.initMocks(this);
+
         productDao = new ArrayListProductDao();
+        productDao.getProductList().add(product1);
+        productDao.getProductList().add(product3);
+
+        when(product1.getId()).thenReturn(1L);
+        when(product1.getPrice()).thenReturn(new BigDecimal(100));
+        when(product1.getStock()).thenReturn(100);
+
+        when(product2.getId()).thenReturn(2L);
+
+        when(product3.getId()).thenReturn(3L);
+        when(product3.getPrice()).thenReturn(null);
+        when(product3.getStock()).thenReturn(300);
     }
 
     @Test
-    public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
+    public void testGetProduct() {
+        assertEquals(product1.getId(), productDao.getProduct(1L).getId());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetProductNullId() {
+        productDao.getProduct(null);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testGetNonExistentProduct() {
+        productDao.getProduct(2L);
+    }
+
+    @Test
+    public void testFindProducts() {
+        assertEquals(productDao.findProducts(), Collections.singletonList(product1));
+    }
+
+    @Test
+    public void testSave() {
+        productDao.save(product2);
+        assertTrue(productDao.getProductList().contains(product2));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSaveAlreadyExistentProduct() {
+        productDao.save(product1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSaveProductNullId() {
+        productDao.save(null);
+    }
+
+    @Test
+    public void testDelete() {
+        productDao.delete(1L);
+        productDao.delete(3L);
+        assertTrue(productDao.getProductList().isEmpty());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testDeleteNonExistentProduct() {
+        productDao.delete(5L);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDeleteProductNullId() {
+        productDao.delete(null);
     }
 }
